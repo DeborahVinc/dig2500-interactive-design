f71e9e8024e683a0c242505fa3e035ce889165a0
Deborah Vincent <149093230+DeborahVinc@users.noreply.github.com>
2026-05-29 23:47:12 -0400

    Update capitalization.js

--- project-2-javascript-skipcount-and-capitalization-demo/scripts/capitalization.js
+++ project-2-javascript-skipcount-and-capitalization-demo/scripts/capitalization.js
@@ -1,1 +1,25 @@
+// JavaScript source code
+// Variables
+const capitalizationInputText = document.getElementById("capitalizationInput");
+const uppercase = document.getElementById("toUppercaseButton");
+const lowercase = document.getElementById("toLowercaseButton");
 
+// Function to uppercase the text
+uppercase.addEventListener("click", function () {
+    // Alerts if nothing in text
+    if (capitalizationInputText.value == "") {
+        alert("Enter a phrase to change capitalization for.\n\nPlease try again.");
+    }
+    // Displays text to uppercase
+    capitalizationInputText.value = capitalizationInputText.value.toUpperCase();
+});
+
+// Function to lowercase the text
+lowercase.addEventListener("click", function () {
+    // Alerts in nothing in text
+    if (capitalizationInputText.value == "") {
+        alert("Enter a phrase to change chapitalization for.\n\nPlease try again.");
+    }
+    // Displays text to lowercase
+    capitalizationInputText.value = capitalizationInputText.value.toLowerCase();
+});
