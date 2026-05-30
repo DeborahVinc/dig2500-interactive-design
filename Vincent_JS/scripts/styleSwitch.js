befc3915ecfac42abb1cb886e564b843826bfd39
Deborah Vincent <149093230+DeborahVinc@users.noreply.github.com>
2026-05-29 23:30:13 -0400

    Upload Project 2 JavaScript files dig 2500

---
+++ Vincent_JS/scripts/styleSwitch.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+// Variable
+const stylesButton = document.getElementById("toChangeStylesButton");
+
+// Function to swtich stype of the website with alernate.css
+stylesButton.addEventListener("click", function () {
+    document.body.classList.toggle("alernateStyle");
+
+});
